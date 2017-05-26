69e1038c2537977241529a1ba5b26ec10ba56dbd
leventnetenets <levent@netenets.com>
2017-05-26 16:31:07 +0300

    Promisifying probe function

--- ping.js
+++ ping.js
@@ -22,24 +22,24 @@
 
 var net = require('net');
 
-var ping = function(options, callback) {
+var ping = function (options, callback) {
     var i = 0;
     var results = [];
     options.address = options.address || 'localhost';
     options.port = options.port || 80;
     options.attempts = options.attempts || 10;
     options.timeout = options.timeout || 5000;
-    var check = function(options, callback) {
-        if(i < options.attempts) {
+    var check = function (options, callback) {
+        if (i < options.attempts) {
             connect(options, callback);
         } else {
-            var avg = results.reduce(function(prev, curr) {
+            var avg = results.reduce(function (prev, curr) {
                 return prev + curr.time;
             }, 0);
-            var max = results.reduce(function(prev, curr) {
+            var max = results.reduce(function (prev, curr) {
                 return (prev > curr.time) ? prev : curr.time;
             }, results[0].time);
-            var min = results.reduce(function(prev, curr) {
+            var min = results.reduce(function (prev, curr) {
                 return (prev < curr.time) ? prev : curr.time;
             }, results[0].time);
             avg = avg / results.length;
@@ -56,25 +56,25 @@ var ping = function(options, callback) {
         }
     };
 
-    var connect = function(options, callback) {
+    var connect = function (options, callback) {
         var s = new net.Socket();
         var start = process.hrtime();
-        s.connect(options.port, options.address, function() {
+        s.connect(options.port, options.address, function () {
             var time_arr = process.hrtime(start);
             var time = (time_arr[0] * 1e9 + time_arr[1]) / 1e6;
-            results.push({ seq: i, time: time });
+            results.push({seq: i, time: time});
             s.destroy();
             i++;
             check(options, callback);
         });
-        s.on('error', function(e) {
-            results.push({seq: i, time: undefined, err: e });
+        s.on('error', function (e) {
+            results.push({seq: i, time: undefined, err: e});
             s.destroy();
             i++;
             check(options, callback);
         });
-        s.setTimeout(options.timeout, function() {
-            results.push({seq: i, time: undefined, err: Error('Request timeout') });
+        s.setTimeout(options.timeout, function () {
+            results.push({seq: i, time: undefined, err: Error('Request timeout')});
             s.destroy();
             i++;
             check(options, callback);
@@ -85,11 +85,14 @@ var ping = function(options, callback) {
 
 module.exports.ping = ping;
 
-module.exports.probe = function(address, port, callback) {
-    address = address || 'localhost';
-    port = port || 80;
-    ping({ address: address, port: port, attempts: 1, timeout: 5000 }, function(err, data) {
-        var available = data.min !== undefined;
-        callback(err, available);
+module.exports.probe = function (address, port) {
+    return new Promise((resolve, reject) => {
+        address = address || 'localhost';
+        port = port || 80;
+        ping({address: address, port: port, attempts: 1, timeout: 5000}, function (err, data) {
+            var available = data.min !== undefined;
+            if(err) return reject(err);
+            return resolve(available);
+        });
     });
 };
